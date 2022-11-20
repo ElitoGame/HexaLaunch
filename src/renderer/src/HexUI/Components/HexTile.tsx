02439bd496c376d0ad3263eb52bba5bb249b886d
ElitoGame <elitogame123@gmail.com>
2022-11-21 00:09:23 +0100

    Animation tests
    - Started working on the animations of the Hexagons. The central hexagons have a spiral opening animation and the outer hexagons have a simple pop up animation using the pythagoras a square + b square = c square formula. Animations should be toggleable, speed adjustable and if I find multiple kinda good looking ones, maybe we can add a choice for users.
    - Changed the coordinate origins of all hexagons. They now start (0,0) in the center, rather then at the first hexagon (1, 0 now).
    - To call the animation each time the UI is being opened I wrapped the inner UI in a Show component to re-trigger the HexTile construction and I used a timeout of 0.1s to guarantee the animation will play every time.

--- src/renderer/src/HexUI/Components/HexTile.tsx
+++ src/renderer/src/HexUI/Components/HexTile.tsx
@@ -1,21 +1,54 @@
+import { createSignal } from 'solid-js';
 import { JSX } from 'solid-js/jsx-runtime';
 import { getHexMargin, getHexSize } from '../../renderer';
 
 const HexTile = (props: {
   x: number;
   y: number;
+  radiant: number;
   onClick?: JSX.EventHandlerUnion<HTMLDivElement, MouseEvent> | undefined;
   zIndex?: number;
   color?: string;
   title?: string;
 }) => {
+  const [getScale, setScale] = createSignal(0);
+  let delay = 0;
+  // give a delay according to these coordinates
+  // 1, 0 -> 1
+  // 1, 1 -> 2
+  // 0, 1 -> 3
+  // -1, 0 -> 4
+  // 0, -1 -> 5
+  // 1, -1 -> 6
+  if (props.radiant === 0) {
+    if (props.x === 1 && props.y === 0) {
+      delay = 1;
+    } else if (props.x === 1 && props.y === 1) {
+      delay = 2;
+    } else if (props.x === 0 && props.y === 1) {
+      delay = 3;
+    } else if (props.x === -1 && props.y === 0) {
+      delay = 4;
+    } else if (props.x === 0 && props.y === -1) {
+      delay = 5;
+    } else if (props.x === 1 && props.y === -1) {
+      delay = 6;
+    }
+  } else {
+    delay = Math.sqrt(props.x * props.x + props.y * props.y) * 1.5;
+  }
+
+  setTimeout(() => {
+    setScale(100);
+  }, 100);
+
   return (
     <div
-      class={'absolute bg-transparent cursor-pointer inline-block'}
+      class={`absolute bg-transparent cursor-pointer inline-block transition-transform`}
       style={{
         left: `${
           props.x * (getHexSize() + getHexMargin()) -
-          (props.y % 2 === 0 ? 0 : (getHexSize() + getHexMargin()) / 2) +
+          (props.y % 2 === 0 ? 0 : (getHexSize() + getHexMargin()) / 2) -
           getHexSize() / 2
         }px`,
         bottom: `${props.y * (getHexSize() * 0.86 + getHexMargin()) - (getHexSize() / 13) * 8}px`,
@@ -24,6 +57,9 @@ const HexTile = (props: {
         height: `${(getHexSize() + getHexMargin()) * 1.169}px`,
         'clip-path': 'polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%)',
         'z-index': props.zIndex ?? 0,
+        'transform-origin': 'center',
+        transform: `scale(${getScale() / 100})`,
+        'transition-duration': `${delay * 0.075}s`,
       }}
     >
       <div
